92b4c9f8bf1e3374105b5d1eea77cd0a9520aa72
RogerioLira <rgsliras@gmail.com>
2023-09-21 18:04:37 -0300

    fazendo nova configuração

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_221511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "icps", force: :cascade do |t|
+    t.string "name"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -33,8 +40,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_15_221511) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.bigint "icp_id"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["icp_id"], name: "index_users_on_icp_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "icps"
 end
