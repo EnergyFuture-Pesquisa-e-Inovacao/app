c151f2cfd183d98b90bbff72c3489f344b32a123
RogerioLira <rgsliras@gmail.com>
2023-10-05 16:11:37 -0300

    add make programasetorial

--- app/models/programasetorial.rb
+++ app/models/programasetorial.rb
@@ -9,4 +9,54 @@ class Programasetorial < ApplicationRecord
                   :category,
                   :enviarparaparticipante
 
+
+    after_create :notify_registrationprogsetcr
+
+    after_update :notify_registrationprogsetup
+                
+    def notify_registrationprogsetcr
+      Integromat.configure do |cprogset|
+       cprogset.web_hooks = { appv3programasetorial: "sj3td7a4iact5qfe9oy2edc5nv49s4lw"}
+       cprogset.base_uri = "https://hook.us1.make.com/"
+      end 
+      Integromat::Webhook.new(:appv3programasetorial).trigger(id: id,
+                                                     name: name,
+                                                     descricao: descricao,
+                                                     descricaocurta: descricaocurta,
+                                                     status: status,
+                                                     ano: ano,
+                                                     empresa: empresa,
+                                                     parceiro: parceiro,
+                                                     quantempresas: quantempresas,
+                                                     quantengajamento: quantengajamento,
+                                                     quantinscricoes: quantinscricoes,
+                                                     descricaocurta: descricaocurta,
+                                                     created_at: created_at,
+                                                    "tipoacao": "create")
+    end
+                
+    def notify_registrationprogsetup
+      Integromat.configure do |cprogset|
+       cprogset.web_hooks = { appv3programasetorial: "sj3td7a4iact5qfe9oy2edc5nv49s4lw"}
+       cprogset.base_uri = "https://hook.us1.make.com/"
+      end 
+      Integromat::Webhook.new(:appv3programasetorial).trigger(id: id,
+                                                     name: name,
+                                                     descricao: descricao,
+                                                     descricaocurta: descricaocurta,
+                                                     status: status,
+                                                     ano: ano,
+                                                     empresa: empresa,
+                                                     parceiro: parceiro,
+                                                     quantempresas: quantempresas,
+                                                     quantengajamento: quantengajamento,
+                                                     quantinscricoes: quantinscricoes,
+                                                     descricaocurta: descricaocurta,
+                                                     updated_at: updated_at,
+                                                     "tipoacao": "update")
+    end
+                               
+
 end
+
+
