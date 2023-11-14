4752f73eb4f00e89c598fe36bbbec44235f273a9
RogerioLira <rgsliras@gmail.com>
2023-11-14 19:18:28 -0300

    fix autorização user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
-    before_action :authenticate_admin!
+    #before_action :authenticate_user!
+    #before_action :authenticate_admin!
     def not_found_method
         render file: Rails.public_path.join('404.html'), status: :not_found, layout: true
     end
